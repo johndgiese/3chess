32c47182ca5a4e2884bdbe810956313102119b17
J. David Giese <johndgiese@gmail.com>
2021-10-20 23:19:16 -0400

    Commit firebase project
    Nearly all of this was auto generated

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBS4QtZaFgiF8Aez_B5Mqs70IJaB2jcZbo",
+  authDomain: "chess-three.firebaseapp.com",
+  projectId: "chess-three",
+  storageBucket: "chess-three.appspot.com",
+  messagingSenderId: "1018144644540",
+  appId: "1:1018144644540:web:1a59994066f1e95764dfb9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
